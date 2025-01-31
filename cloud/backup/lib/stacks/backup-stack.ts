4de0bcabc6f18d346a83dcbeca0a9566c92026f3
Lucas Messenger <1335960+layertwo@users.noreply.github.com>
2025-01-30 18:16:20 -0800

    feat(cdktf): add s3 backend

--- cloud/backup/lib/stacks/backup-stack.ts
+++ cloud/backup/lib/stacks/backup-stack.ts
@@ -1,42 +1,33 @@
-import {TerraformStack, TerraformVariable} from "cdktf";
+import {S3Backend, TerraformStack, TerraformVariable} from "cdktf";
 import {Construct} from "constructs";
 
 import {AwsProvider} from "../../.gen/providers/aws/provider";
 import {S3Bucket} from "../../.gen/providers/aws/s3-bucket";
 
 export class BackupStack extends TerraformStack {
+    private readonly accountId: TerraformVariable;
+    private readonly accessKey: TerraformVariable;
+    private readonly secretKey: TerraformVariable;
+
     constructor(scope: Construct, id: string) {
         super(scope, id);
 
-        const accountId = new TerraformVariable(this, "CLOUDFLARE_ACCOUNT_ID", {
+        this.accountId = new TerraformVariable(this, "CLOUDFLARE_ACCOUNT_ID", {
             type: "string",
             description: "Cloudflare account ID",
         });
 
-        const accessKey = new TerraformVariable(this, "R2_ACCESS_KEY", {
+        this.accessKey = new TerraformVariable(this, "AWS_ACCESS_KEY", {
             type: "string",
-            description: "R2 AWS access key",
+            description: "AWS access key",
         });
 
-        const secretKey = new TerraformVariable(this, "R2_SECRET_KEY", {
+        this.secretKey = new TerraformVariable(this, "AWS_SECRET_KEY", {
             type: "string",
-            description: "R2 AWS secret key",
+            description: "AWS secret key",
         });
-
-        new AwsProvider(this, "R2Provider", {
-            accessKey: accessKey.value,
-            secretKey: secretKey.value,
-            region: "WNAM", // Western North America,
-            skipCredentialsValidation: true,
-            skipRegionValidation: true,
-            skipRequestingAccountId: true,
-            s3UsePathStyle: true,
-            endpoints: [
-                {
-                    s3: `https://${accountId.value}.r2.cloudflarestorage.com`,
-                },
-            ],
-        });
+        this.setupProvider();
+        this.setupBackend();
 
         new S3Bucket(this, "Volsync", {
             bucket: "layertwo-dev-volsync",
@@ -44,6 +35,41 @@ export class BackupStack extends TerraformStack {
 
         new S3Bucket(this, "CloudNativePG", {
             bucket: "layertwo-dev-cloudnativepg",
+        });
+    }
+
+    private setupProvider(): AwsProvider {
+        return new AwsProvider(this, "R2Provider", {
+            accessKey: this.accessKey.value,
+            secretKey: this.secretKey.value,
+            region: "WNAM", // Western North America,
+            skipCredentialsValidation: true,
+            skipRegionValidation: true,
+            skipRequestingAccountId: true,
+            s3UsePathStyle: true,
+            endpoints: [
+                {
+                    s3: `https://${this.accountId.value}.r2.cloudflarestorage.com`,
+                },
+            ],
+        });
+    }
+
+    private setupBackend(): S3Backend {
+        return new S3Backend(this, {
+            bucket: "layertwo-dev-tofu",
+            key: "r2/tf.state",
+            accessKey: this.accessKey.value,
+            secretKey: this.secretKey.value,
+            region: "WNAM",
+            skipCredentialsValidation: true,
+            skipRegionValidation: true,
+            skipRequestingAccountId: true,
+            skipS3Checksum: true,
+            usePathStyle: true,
+            endpoints: {
+                s3: `https://${this.accountId.value}.r2.cloudflarestorage.com`,
+            },
         });
     }
 }
