a8221ddbf19094542277620da7262a345e5c75ef
Lucas Messenger <1335960+layertwo@users.noreply.github.com>
2025-02-28 22:04:53 -0800

    feat: add cloudflare provider to cdktf

--- cloud/cdktf/lib/constructs/base-stack.ts
+++ cloud/cdktf/lib/constructs/base-stack.ts
@@ -2,14 +2,22 @@ import {S3Backend, TerraformStack, TerraformVariable} from "cdktf";
 import {Construct} from "constructs";
 
 import {AwsProvider} from "../../.gen/providers/aws/provider";
+import {CloudflareProvider} from "../../.gen/providers/cloudflare/provider";
+
+export interface BaseStackProps {
+    backupKey: string;
+}
 
 export class BaseStack extends TerraformStack {
+    private readonly props: BaseStackProps;
+
     private readonly accountId: TerraformVariable;
     private readonly accessKey: TerraformVariable;
     private readonly secretKey: TerraformVariable;
 
-    constructor(scope: Construct, id: string) {
+    constructor(scope: Construct, id: string, props: BaseStackProps) {
         super(scope, id);
+        this.props = props;
 
         this.accountId = new TerraformVariable(this, "CLOUDFLARE_ACCOUNT_ID", {
             type: "string",
@@ -25,12 +33,13 @@ export class BaseStack extends TerraformStack {
             type: "string",
             description: "AWS secret key",
         });
-        this.setupProvider();
+        this.setupAwsProvider();
+        this.setupCloudflareProvider();
         this.setupBackend();
     }
 
-    private setupProvider(): AwsProvider {
-        return new AwsProvider(this, "R2Provider", {
+    private setupAwsProvider(): AwsProvider {
+        return new AwsProvider(this, "AwsProvider", {
             accessKey: this.accessKey.value,
             secretKey: this.secretKey.value,
             region: "WNAM", // Western North America,
@@ -46,10 +55,20 @@ export class BaseStack extends TerraformStack {
         });
     }
 
+    private setupCloudflareProvider(): CloudflareProvider {
+        const apiToken = new TerraformVariable(this, "CLOUDFLARE_API_TOKEN", {
+            type: "string",
+            description: "Cloudflare API token",
+        });
+        return new CloudflareProvider(this, "CloudflareProvider", {
+            apiToken: apiToken.value,
+        });
+    }
+
     private setupBackend(): S3Backend {
         return new S3Backend(this, {
             bucket: "layertwo-dev-tofu",
-            key: "r2/tf.state",
+            key: `${this.props.backupKey}/tf.state`,
             accessKey: this.accessKey.value,
             secretKey: this.secretKey.value,
             region: "WNAM",
