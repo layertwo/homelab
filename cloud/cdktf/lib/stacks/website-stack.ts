5ea49ae1dad2223b052042e16f45234ec751d689
Lucas Messenger <1335960+layertwo@users.noreply.github.com>
2025-03-01 22:01:57 -0800

    feat: add website stack

--- cloud/cdktf/lib/stacks/website-stack.ts
+++ cloud/cdktf/lib/stacks/website-stack.ts
@@ -1,5 +1,6 @@
 import {R2Bucket} from "@cdktf/provider-cloudflare/lib/r2-bucket";
-// import {Ruleset} from "@cdktf/provider-cloudflare/lib/ruleset";
+import {Ruleset} from "@cdktf/provider-cloudflare/lib/ruleset";
+import {TerraformVariable, Token} from "cdktf";
 import {Construct} from "constructs";
 
 import {ObjectResource} from "../../.gen/providers/restapi/object-resource";
@@ -8,19 +9,27 @@ import {BaseStack} from "../constructs/base-stack";
 
 export class WebsiteStack extends BaseStack {
     private readonly customCloudflareProvider: RestapiProvider;
-    private readonly zoneId: string;
-    private readonly domainName: string;
+    private readonly zoneId: TerraformVariable;
+    private readonly domainName: TerraformVariable;
     private readonly bucket: R2Bucket;
 
     constructor(scope: Construct, id: string) {
         super(scope, id, {backupKey: "website"});
 
-        this.zoneId = "3872e2e4571cd46c72056b4cbb89e588";
-        this.domainName = "layertwo.dev";
+        this.zoneId = new TerraformVariable(this, "CLOUDFLARE_ZONE_ID", {
+            type: "string",
+            description: "Cloudflare zone ID",
+        });
+
+        this.domainName = new TerraformVariable(this, "DOMAIN_NAME", {
+            type: "string",
+            description: "Domain name",
+        });
+
         this.customCloudflareProvider = this.setupCustomCloudflareProvider();
         this.bucket = this.createWebsiteBucket();
-        this.createR2CustomDomain();
-        // this.createRuleset();
+        this.createR2CustomDomains();
+        this.createRuleset();
     }
     private createWebsiteBucket(): R2Bucket {
         return new R2Bucket(this, "WebsiteBucket", {
@@ -40,42 +49,47 @@ export class WebsiteStack extends BaseStack {
         });
     }
 
-    private createR2CustomDomain(): void {
-        const domain = `www.${this.domainName}`;
-        new ObjectResource(this, "R2CustomDomainObjectResource", {
-            provider: this.customCloudflareProvider,
-            objectId: domain,
-            idAttribute: "result/domain",
-            path: `/r2/buckets/${this.bucket.name}/domains/custom`,
-            data: JSON.stringify({
-                domain,
-                zoneId: this.zoneId,
-                enabled: true,
-                minTLS: "1.2",
-            }),
+    private createR2CustomDomains(): void {
+        const prefixes = ["", "www"];
+        const baseDomain = Token.asString(this.domainName.value);
+        prefixes.map((prefix) => {
+            const domain = prefix ? `${prefix}.${baseDomain}` : baseDomain;
+            new ObjectResource(this, `R2CustomDomainObjectResource${prefix.toUpperCase()}`, {
+                forceNew: ["object_id"],
+                provider: this.customCloudflareProvider,
+                objectId: domain,
+                idAttribute: "result/domain",
+                path: `/r2/buckets/${this.bucket.name}/domains/custom`,
+                data: JSON.stringify({
+                    domain,
+                    zoneId: Token.asString(this.zoneId.value),
+                    enabled: true,
+                    minTLS: "1.2",
+                }),
+            });
         });
     }
 
-    // private createRuleset(): Ruleset {
-    //     return new Ruleset(this, "WebsiteRuleset", {
-    //         zoneId: this.zoneId,
-    //         name: "redirect-to-index",
-    //         kind: "zone",
-    //         phase: "http_request_dynamic_redirect",
-    //         rules: [
-    //             {
-    //                 ref: "url_rewrite_index",
-    //                 expression: '(ends_with(http.request.uri.path, "/"))',
-    //                 action: "rewrite",
-    //                 actionParameters: {
-    //                     uri: {
-    //                         path: {
-    //                             expression: 'concat(http.request.uri.path, "index.html")',
-    //                         },
-    //                     },
-    //                 },
-    //             },
-    //         ],
-    //     });
-    // }
+    private createRuleset(): Ruleset {
+        return new Ruleset(this, "WebsiteRuleset", {
+            zoneId: this.zoneId.value,
+            name: "redirect-to-index",
+            kind: "zone",
+            phase: "http_request_transform",
+            rules: [
+                {
+                    ref: "url_rewrite_index",
+                    expression: '(ends_with(http.request.uri.path, "/"))',
+                    action: "rewrite",
+                    actionParameters: {
+                        uri: {
+                            path: {
+                                expression: 'concat(http.request.uri.path, "index.html")',
+                            },
+                        },
+                    },
+                },
+            ],
+        });
+    }
 }
