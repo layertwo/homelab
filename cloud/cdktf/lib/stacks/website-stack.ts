87cd3eb69c4f1b33eb8264e4b6faf2de3f334cac
Lucas Messenger <1335960+layertwo@users.noreply.github.com>
2025-05-10 20:09:00 -0700

    chore(website): rework cdktf for cloudflare website (#830)

--- cloud/cdktf/lib/stacks/website-stack.ts
+++ cloud/cdktf/lib/stacks/website-stack.ts
@@ -1,14 +1,12 @@
 import {R2Bucket} from "@cdktf/provider-cloudflare/lib/r2-bucket";
+import {R2CustomDomain} from "@cdktf/provider-cloudflare/lib/r2-custom-domain";
 import {Ruleset} from "@cdktf/provider-cloudflare/lib/ruleset";
 import {TerraformVariable, Token} from "cdktf";
 import {Construct} from "constructs";
 
-import {ObjectResource} from "../../.gen/providers/restapi/object-resource";
-import {RestapiProvider} from "../../.gen/providers/restapi/provider";
 import {BaseStack} from "../constructs/base-stack";
 
 export class WebsiteStack extends BaseStack {
-    private readonly customCloudflareProvider: RestapiProvider;
     private readonly zoneId: TerraformVariable;
     private readonly domainName: TerraformVariable;
     private readonly bucket: R2Bucket;
@@ -26,7 +24,6 @@ export class WebsiteStack extends BaseStack {
             description: "Domain name",
         });
 
-        this.customCloudflareProvider = this.setupCustomCloudflareProvider();
         this.bucket = this.createWebsiteBucket();
         this.createR2CustomDomains();
         this.createRuleset();
@@ -35,37 +32,23 @@ export class WebsiteStack extends BaseStack {
         return new R2Bucket(this, "WebsiteBucket", {
             accountId: this.accountId.value,
             name: "layertwo-dev-website",
-        });
-    }
-
-    private setupCustomCloudflareProvider(): RestapiProvider {
-        return new RestapiProvider(this, "CloudflareRestapiProvider", {
-            uri: `https://api.cloudflare.com/client/v4/accounts/${this.accountId.value}`,
-            writeReturnsObject: true,
-            headers: {
-                Authorization: `Bearer ${this.cloudflareApiToken.value}`,
-                "Content-Type": "application/json",
-            },
+            location: "wnam",
+            storageClass: "Standard"
         });
     }
 
     private createR2CustomDomains(): void {
         const prefixes = ["", "www"];
-        const baseDomain = Token.asString(this.domainName.value);
+        const baseDomain = this.domainName.value;
         prefixes.map((prefix) => {
             const domain = prefix ? `${prefix}.${baseDomain}` : baseDomain;
-            new ObjectResource(this, `R2CustomDomainObjectResource${prefix.toUpperCase()}`, {
-                forceNew: ["object_id"],
-                provider: this.customCloudflareProvider,
-                objectId: domain,
-                idAttribute: "result/domain",
-                path: `/r2/buckets/${this.bucket.name}/domains/custom`,
-                data: JSON.stringify({
-                    domain,
-                    zoneId: Token.asString(this.zoneId.value),
-                    enabled: true,
-                    minTLS: "1.2",
-                }),
+            new R2CustomDomain(this, `R2CustomDomainObjectResource${prefix.toUpperCase()}`, {
+                accountId: this.accountId.value,
+                bucketName: this.bucket.name,
+                domain,
+                enabled: true,
+                minTls: "1.2",
+                zoneId: this.zoneId.value,
             });
         });
     }
